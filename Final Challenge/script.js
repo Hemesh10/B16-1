0a2eaf15ecd59e94dca4d0591c8051446543b5b7
HemeshThakur <king.hemesh1004@gmail.com>
2022-05-15 20:32:10 +0530

    Changes made on 15/05/22

--- Final Challenge/script.js
+++ Final Challenge/script.js
@@ -1,38 +1,48 @@
+gsap.from(".box h1", {
+    y:"85%",
+    stagger:2,
+    duration:2,
+    ease:Back.easeInOut,
+    opacity:0
+});
+
 gsap.to(".box h1", {
-    y:"0%",
-    stagger:1,
-    duration:1,
+    delay:2,
+    y:"-85%",
+    stagger:2,
+    duration:2,
     ease:Expo.easeInOut,
-    opacity:1
-})
+    opacity:0
+});
 
-$(".box h1").textillate({ in: { effect: 'fadeInUp' } });
-
-/* gsap.to("#main", {
+gsap.to("#main", {
     y:"-50%",
-    delay:4,
+    delay:8,
     duration:2,
     ease:Expo.easeInOut
-}) */
+});
 
 gsap.to(".ttxt h1", {
     y:"0%",
     stagger:.5,
     opacity:1,
     duration:1,
-    delay:5,
+    delay:10,
     ease:Expo.easeInOut
-})
+});
+
+$(".ttxt h1").textillate({ in: { effect: 'fadeInUp' } });
+
 gsap.from("#imagebox", {
     height:"0px",
     duration:1.5,
-    delay:6,
+    delay:11,
     ease:Expo.easeInOut
 })
 
 gsap.to("#nav", {
     y:"0%",
-    delay:6,
+    delay:11,
     duration:1.5,
     ease:Expo.easeInOut,
     opacity:1    
--- JS practice/AnimationPractice/script.js
+++ JS practice/AnimationPractice/script.js
@@ -1,12 +1,4 @@
-gsap.to("#box",{
-    scrollTrigger:{
-        trigger:"#secondmain",
-        start:"5% 50%",
-        end:"30% 50%",
-        scrub: 5,
-        markers:true
-    },
-    y: "50vw",
-    rotate:360,
-    duration:5,    
+gsap.from("#box", {
+    y:"500px",
+    duration:1
 })
